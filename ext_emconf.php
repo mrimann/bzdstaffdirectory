703b88d12fd8af7ddf076eb5340bf171e8f0355e
Mario Rimann <typo3-coding@rimann.org>
2007-05-20 15:03:36 +0000

    Released v0.5.0 of the BZD Staff Directory extension.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "bzd_staff_directory"
 #
-# Auto generated 08-04-2007 10:58
+# Auto generated 20-05-2007 17:03
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'An extension to show your staff or club-members in different output styles (single persons, lists).',
 	'category' => 'plugin',
 	'shy' => 0,
-	'dependencies' => '',
+	'dependencies' => 'cms,bidirectional',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.4.0',
-	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"3144";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"0bb0";s:12:"ext_icon.gif";s:4:"15b9";s:17:"ext_localconf.php";s:4:"4730";s:14:"ext_tables.php";s:4:"a38a";s:14:"ext_tables.sql";s:4:"b74d";s:24:"ext_typoscript_setup.txt";s:4:"124d";s:15:"flexform_ds.xml";s:4:"01a3";s:36:"icon_tx_bzdstaffdirectory_groups.gif";s:4:"401f";s:37:"icon_tx_bzdstaffdirectory_persons.gif";s:4:"78eb";s:13:"locallang.xml";s:4:"5a5c";s:16:"locallang_db.xml";s:4:"44d1";s:7:"tca.php";s:4:"64c1";s:8:"todo.txt";s:4:"b6f5";s:29:"media/bzd_staff_directory.css";s:4:"728a";s:27:"media/bzdstaff_template.htm";s:4:"64d3";s:28:"media/dummyPictureFemale.jpg";s:4:"1361";s:26:"media/dummyPictureMale.jpg";s:4:"f1f4";s:15:"media/noimg.jpg";s:4:"4d3f";s:14:"pi1/ce_wiz.gif";s:4:"9322";s:38:"pi1/class.tx_bzdstaffdirectory_pi1.php";s:4:"6b23";s:46:"pi1/class.tx_bzdstaffdirectory_pi1_wizicon.php";s:4:"1d43";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"c514";s:24:"pi1/static/editorcfg.txt";s:4:"92fe";s:14:"doc/manual.sxw";s:4:"4d23";s:19:"doc/wizard_form.dat";s:4:"9116";s:20:"doc/wizard_form.html";s:4:"78fb";}',
+	'version' => '0.5.0',
+	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"f897";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"0bb0";s:12:"ext_icon.gif";s:4:"15b9";s:17:"ext_localconf.php";s:4:"4730";s:14:"ext_tables.php";s:4:"a38a";s:14:"ext_tables.sql";s:4:"a114";s:24:"ext_typoscript_setup.txt";s:4:"fee2";s:15:"flexform_ds.xml";s:4:"8c93";s:36:"icon_tx_bzdstaffdirectory_groups.gif";s:4:"401f";s:37:"icon_tx_bzdstaffdirectory_persons.gif";s:4:"78eb";s:13:"locallang.xml";s:4:"0c99";s:16:"locallang_db.xml";s:4:"214c";s:7:"tca.php";s:4:"05ab";s:8:"todo.txt";s:4:"b6f5";s:29:"media/bzd_staff_directory.css";s:4:"728a";s:27:"media/bzdstaff_template.htm";s:4:"64d3";s:28:"media/dummyPictureFemale.jpg";s:4:"1361";s:26:"media/dummyPictureMale.jpg";s:4:"f1f4";s:15:"media/noimg.jpg";s:4:"4d3f";s:14:"pi1/ce_wiz.gif";s:4:"9322";s:38:"pi1/class.tx_bzdstaffdirectory_pi1.php";s:4:"c705";s:46:"pi1/class.tx_bzdstaffdirectory_pi1_wizicon.php";s:4:"1d43";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"e98c";s:24:"pi1/static/editorcfg.txt";s:4:"92fe";s:14:"doc/manual.sxw";s:4:"d474";s:19:"doc/wizard_form.dat";s:4:"9116";s:20:"doc/wizard_form.html";s:4:"78fb";}',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -45,6 +45,8 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'suggests' => array(
 		),
+	),
+	'suggests' => array(
 	),
 );
 
